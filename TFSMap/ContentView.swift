e3caf1ae245387c37f1f7f66062c44fecf06eec2
Tyler Wade <tylergwade@gmail.com>
2025-03-09 12:29:01 -0400

    The app is finished!

--- TFSMap/ContentView.swift
+++ TFSMap/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var activeTabIndex = 2
-    
     var body: some View {
         MapView()
-            // This app only supports light mode
-            // so make sure that is enforced.
-            .colorScheme(.light)
     }
 }
 
--- TFSMap/MapView.swift
+++ TFSMap/MapView.swift
@@ -15,6 +15,7 @@ class Room {
     public let position: CGPoint
     
     init(named name: String, at position: CGPoint) {
+        
         self.name = name
         self.position = position
     }
@@ -28,6 +29,7 @@ class Floor {
     public var building: Building? = nil
     
     init(svgNode: SVGNode, level: Int) {
+        
         self.svgNode = svgNode
         self.level = level
     }
@@ -40,36 +42,24 @@ class Building {
     public let roofNode: SVGNode
     public let hitRect: CGRect
     public var floors:[Floor]
-    public let defaultFloor: Int
+    public let defaultFloorIdx: Int
     
     init(
         named name: String, roofNode: SVGNode,
         hitRect: CGRect, floors:[Floor],
         defaultFloor: Int, id: String) {
+            
         self.name = name
         self.roofNode = roofNode
         self.hitRect = hitRect
         self.floors = floors
-        self.defaultFloor = defaultFloor
+        self.defaultFloorIdx = defaultFloor
         self.id = id
     }
 }
 
 struct MapView: View {
     
-    // Coordinate spaces
-    //
-    // Map Space:
-    //   A coordinate that is relative to the map.
-    //   The coordinates go from (0, 0) at the
-    //   top-left and extend out to svgSize.
-    //
-    // Screen Space:
-    //   A coordinate that is relative to the screen.
-    //   The coordinates go from (0, 0) at the
-    //   top-left and and go to viewSize
-    //
-
     // The zoom scale at which the text on the buildings apear
     private static let textAppearScale = 7.0
     
@@ -98,11 +88,18 @@ struct MapView: View {
     
     private var buildings:[Building] = []
     
+    @State private var selectedFloorIdx: Int = 0
+    
     init() {
         
         // Load the SVG view
         let svgURL = Bundle.main.url(forResource: "TandemMap", withExtension: "svg")!
         self.svgView = SVGView(contentsOf: svgURL)
+    
+        initializeBuildingsAndRooms()
+    }
+    
+    mutating func initializeBuildingsAndRooms() {
         
         // Initialize buildings
         var main = addBuilding(
@@ -130,20 +127,109 @@ struct MapView: View {
             named: "Field House/Gym", id: "Gym",
             numFloors: 1, defaultFloor: 0)
         
-        // Initialize room names in each building
+        // Initialize rooms in each building
+        
+        // Pavilion
         addRoom(pavilion.floors[0], "Storage", "Storage")
+        addRoom(pavilion.floors[0], "Restroom", "Rest1")
+        addRoom(pavilion.floors[0], "Restroom", "Rest2")
+        
+        // Community Hall
+        addRoom(community.floors[0], "Stairs", "Stairs")
+        addRoom(community.floors[0], "Girls Restroom", "FRest")
+        addRoom(community.floors[0], "Boys Restroom", "MRest")
+        addRoom(community.floors[0], "Office", "Office")
+        addRoom(community.floors[0], "Allende", "Allende")
+        addRoom(community.floors[0], "Cafe", "Cafe")
+        addRoom(community.floors[0], "Hurston", "Hurst")
+        addRoom(community.floors[0], "Renaissance", "Ren")
+        addRoom(community.floors[0], "Martin", "Martin")
+        
+        addRoom(community.floors[1], "Community Hall", "Hall")
+        addRoom(community.floors[1], "Lobby", "Lobby")
+        addRoom(community.floors[1], "Boys Restroom", "MRest")
+        addRoom(community.floors[1], "Girls Restroom", "FRest")
+        addRoom(community.floors[1], "Costume Closet", "Closet")
+        addRoom(community.floors[1], "Backstage", "Backstage")
+        
+        // Math/Science building
+        addRoom(math.floors[0], "Einstein", "Einstein")
+        addRoom(math.floors[0], "Restrooms", "Rest")
+        addRoom(math.floors[0], "Stairs", "Stairs")
+        addRoom(math.floors[0], "Veranda", "Veranda")
+        addRoom(math.floors[0], "Avogadro", "Avogadro")
+        addRoom(math.floors[0], "Darwin", "Darwin")
+        addRoom(math.floors[0], "Storage", "Storage")
+        
+        addRoom(math.floors[1], "Stairs", "Stairs")
+        addRoom(math.floors[1], "Girls Restroom", "FRest")
+        addRoom(math.floors[1], "Boys Restroom", "MRest")
+        addRoom(math.floors[1], "Gauss", "Gauss")
+        addRoom(math.floors[1], "Newton", "Newton")
+        addRoom(math.floors[1], "Euler", "Euler")
+        addRoom(math.floors[1], "Euclid", "Euclid")
+        addRoom(math.floors[1], "Perk", "Perk")
+        addRoom(math.floors[1], "Library", "Library")
+        
+        // Gym
+        addRoom(gym.floors[0], "Office", "Office")
+        addRoom(gym.floors[0], "Visitor", "Visitor")
+        addRoom(gym.floors[0], "Gym", "Gym")
+        addRoom(gym.floors[0], "Weight Room", "Weight")
+        addRoom(gym.floors[0], "Girls Locker Room", "FLocker")
+        addRoom(gym.floors[0], "Boys Locker Room", "MLocker")
+        
+        // Music building
+        addRoom(music.floors[0], "US Music", "US")
+        addRoom(music.floors[0], "MS Music", "MS")
+        
+        // Main building
+        addRoom(main.floors[0], "Admissions Office", "Admissions")
+        addRoom(main.floors[0], "Lobby", "Lobby")
+        addRoom(main.floors[0], "Faculty Lounge", "TeacherLounge")
+        addRoom(main.floors[0], "Stairs", "Stairs")
+        addRoom(main.floors[0], "Restroom", "Rest")
+        addRoom(main.floors[0], "Senior Lounge", "SeniorLounge")
+        addRoom(main.floors[0], "Alling", "Alling")
+        addRoom(main.floors[0], "Blue Room", "Blue")
+        
+        addRoom(main.floors[1], "Head of School Office", "Head")
+        addRoom(main.floors[1], "Assistant Office", "Admin")
+        addRoom(main.floors[1], "Office", "Office")
+        addRoom(main.floors[1], "LRC", "LRC")
+        addRoom(main.floors[1], "College Counseling Office", "College")
+        
+        // Middle school building
+        addRoom(middle.floors[0], "Restroom", "Rest")
+        addRoom(middle.floors[0], "Porter", "Porter")
+        addRoom(middle.floors[0], "Meyer", "Meyer")
+        
+        addRoom(middle.floors[1], "Summers", "Summers")
+        addRoom(middle.floors[1], "Macdonald", "Mac")
+        addRoom(middle.floors[1], "Henrikson", "Henrikson")
+        addRoom(middle.floors[1], "Carson", "Carson")
+        
+        // Art building
+        addRoom(art.floors[0], "Art Room", "Art")
+        addRoom(art.floors[0], "Ceramics", "Ceramics")
+        addRoom(art.floors[0], "Office", "Office")
+        addRoom(art.floors[0], "US Counseling Office", "ConUS")
+        addRoom(art.floors[0], "Makerspace", "Makerspace")
+        
+        addRoom(art.floors[1], "MS Counseling Office", "ConMS")
     }
     
     mutating func addRoom(_ floor: Floor, _ name: String, _ id: String) {
         // For some reason the compiler cannot type check
         // this expression in reasonable time?????
-        //let roomId = floor.building!.name + "_F" + String(floor.level) + "_" + id;
+        //let roomId = floor.building!.id + "_F" + String(floor.level) + "_" + id
         
-        let part1 = floor.building!.name + "_F"
+        let part1 = floor.building!.id + "_F"
         let part2 = String(floor.level) + "_" + id
         let roomId = part1 + part2
         
         let node = getNode(named: roomId) as! SVGCircle
+        node.opacity = 0
         floor.rooms.append(Room(named: name, at: CGPoint(x: node.cx, y: node.cy)))
     }
     
@@ -252,7 +338,7 @@ struct MapView: View {
             // Show the roof of the building that was previously active
             if activeBuilding !== nil {
                 activeBuilding!.roofNode.opacity = 1
-                activeBuilding!.floors[0].svgNode.opacity = 0
+                activeBuilding!.floors[selectedFloorIdx].svgNode.opacity = 0
             }
             
             activeBuilding = tappedBuilding
@@ -260,10 +346,27 @@ struct MapView: View {
             // Hide the roof that is now currently active
             if tappedBuilding !== nil {
                 tappedBuilding!.roofNode.opacity = 0
-                tappedBuilding!.floors[0].svgNode.opacity = 1
+                tappedBuilding!.floors[tappedBuilding!.defaultFloorIdx].svgNode.opacity = 1
+            }
+            
+            if activeBuilding !== nil {
+                selectedFloorIdx = activeBuilding!.defaultFloorIdx
+            }
+        }
+        else if activeBuilding !== nil && activeBuilding!.floors.count > 1 {
+            if selectedFloorIdx + 1 >= activeBuilding!.floors.count {
+                setFloor(index: 0)
+            } else {
+                setFloor(index: selectedFloorIdx + 1)
             }
         }
         
+    }
+    
+    func setFloor(index: Int) {
+        activeBuilding!.floors[selectedFloorIdx].svgNode.opacity = 0
+        selectedFloorIdx = index
+        activeBuilding!.floors[index].svgNode.opacity = 1
     }
     
     func computeTextOpacity() -> CGFloat {
@@ -288,6 +391,34 @@ struct MapView: View {
             .font(font)
             .foregroundStyle(.white.opacity(textOpacity))
         context.draw(context.resolve(text), at: point, anchor: .center)
+    }
+    
+    var popupContent: some View {
+        HStack {
+            Text(activeBuilding?.name ?? "")
+                .font(.system(size: 20, weight: .bold))
+            
+            Spacer()
+            
+            Text("Floor " + String(selectedFloorIdx + 1))
+                .font(.system(size: 20, weight: .regular))
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+    }
+    
+    var popupFrame: some View {
+        VStack {
+            RoundedRectangle(cornerSize: CGSize(width: 15, height: 15))
+                .fill(Color.white)
+                .frame(width: 300, height: 50)
+                .overlay {
+                    popupContent
+                }
+            
+            Spacer()
+        }
+        .padding()
     }
     
     var body: some View {
@@ -334,28 +465,25 @@ struct MapView: View {
                         
                         drawText(&context, building.name, screenPoint)
                     }
+                    
+                    if activeBuilding !== nil {
+                        let floor = activeBuilding!.floors[selectedFloorIdx]
+                        for room in floor.rooms {
+                            let screenPoint = MapToScreenSpace(room.position)
+                            drawText(&context, room.name, screenPoint)
+                        }
+                    }
                 }
             }
             
             if activeBuilding !== nil {
-                VStack {
-                    RoundedRectangle(cornerSize: CGSize(width: 15, height: 15))
-                        .fill(Color.white)
-                        .frame(width: 300, height: 80)
-                        .overlay {
-                            Text(activeBuilding!.name)
-                                .font(.system(size: 20, weight: .bold))
-                        }
-                    
-                    Spacer()
-                }
-                .padding()
+                popupFrame
             }
         }
         .transformGesture(transform: transform, draggingDisabled: true, onTap: onTapped)
     }
 }
 
-#Preview {
-    MapView()
-}
+//#Preview {
+//    MapView()
+//}
